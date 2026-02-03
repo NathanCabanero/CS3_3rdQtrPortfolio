fed926f517534dfa7aa83ce129701a3d8103ffab
NathanCabanero <b2029napcabanero@pshs.edu.ph>
2026-02-03 08:15:46 +0800

    Done

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function changeColor(ele) {
+    console.log(ele);
+    ele.style.backgroundColor = "lightblue";
+}
+function resetColor(ele) {
+    console.log(ele);
+    ele.style.backgroundColor = "white";
+}
